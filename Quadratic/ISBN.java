f61b6b45d6ca2a24c90021c2c22151d92aeacde6
Dalton Dayhoff <dalton.dayhoff@sdl.usu.edu>
2022-06-30 09:00:19 -0600

    added ISBN file

---
+++ Quadratic/ISBN.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ISBN {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first 9 digits of an ISBN: ");
+        int firstNine = input.nextInt();
+
+    }
+}
